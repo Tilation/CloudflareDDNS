fe300110bf758a32f9dbdd1c87d1ab42c11c11a2
Tilation <48564654+Tilation@users.noreply.github.com>
2023-12-07 18:45:45 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+
+namespace CloudflareDDNS
+{
+    internal class Program
+    {
+        struct newip
+        {
+            public string ip;
+        }
+        static void Main(string[] args)
+        {
+            if (!File.Exists("settings.json"))
+            {
+                File.WriteAllText("settings.json", JsonConvert.SerializeObject(new Settings(), Formatting.Indented));
+                Console.WriteLine("Created settings.json, please edit!");
+                return;
+            }
+
+            Settings settings = JsonConvert.DeserializeObject<Settings>(File.ReadAllText("settings.json"));
+
+            if (settings == null)
+            {
+                Console.WriteLine("Invalid configuration file!");
+                return;
+            }
+
+            HttpClient client = new HttpClient();
+
+            if (settings.AutoGetPublicIp)
+            {
+                var val = client.GetAsync("https://api.ipify.org?format=json").Result;
+                if (val.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    var _newip = JsonConvert.DeserializeObject<newip>(val.Content.ReadAsStringAsync().Result);
+                    settings.NewIp = _newip.ip; 
+                }
+            }
+
+
+            // Create the HttpContent for the form to be posted.
+            var requestContent = new FormUrlEncodedContent(new Dictionary<string, string> {
+                ["content"] = settings.NewIp,
+                ["name"] = settings.DomainName,
+                ["proxied"] = settings.Proxied.ToString().ToLower(),
+                ["type"] = settings.Type,
+                ["comment"] = settings.Comment,
+                ["ttl"] = settings.TTL.ToString(),
+            });
+
+            // Get the response.
+            HttpResponseMessage response = client.PutAsync(
+                $"https://api.cloudflare.com/client/v4/zones/{settings.ZoneIdentifier}/dns_records/{settings.Identifier}",
+                requestContent).Result;
+
+            // Get the response content.
+            var result = response.Content.ReadAsStringAsync().Result;
+
+            // Write the output.
+            Console.WriteLine(result);
+            
+        }
+    }
+}
---
+++ Settings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudflareDDNS
+{
+    internal class Settings
+    {
+        public string DomainName { get; set; } = "your.domain.com";
+        public bool Proxied { get; set; } = false;
+        public string Type { get; set; } = "A";
+        public string Comment { get; set; } = "a domain";
+        public int TTL { get; set; } = 3600;
+        public string NewIp { get; set; } = "your.public.ip";
+        public string Identifier { get; set; } = "023e105f4ecef8ad9ca31a8372d0c353";
+        public string ZoneIdentifier { get; set; } = "023e105f4ecef8ad9ca31a8372d0c353";
+
+        public bool AutoGetPublicIp { get; set; } = true; 
+    }
+}
